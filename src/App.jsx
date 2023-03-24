7561891334494f5cecc262edf503d7694c6412e2
BrianDCraw <briandcraw@gmail.com>
2023-03-24 05:53:43 -0700

    some code refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import GridButton from "./GridButton";
 import React, { Component } from 'react';
 import axios from "axios";
+import * as _ from "lodash";
 
 class App extends Component {
   constructor(props) {
@@ -15,7 +16,7 @@ class App extends Component {
 
     let newState = this.state; //Grab state which is where the JSON is saved
     var addlight = true; //Assume we are adding not removing a light
-     console.log(newState);
+
     //loop through each light in json and see if the coordinate exists
     newState.article.lights.forEach( (light, index) => {
       if(JSON.stringify(light.lightNum) == JSON.stringify(LightNum))
@@ -23,16 +24,19 @@ class App extends Component {
           //light arleady in Array
           addlight =false
           newState.article.lights.splice(index,1); //Remove coordinate from array
+          let lightindex = newState.LEDS.findIndex (el => el.LightNum === LightNum)
           //set the incoming RGB to 0,0,0 so we can turn off the light
           RGB[0] = 0
           RGB[1] = 0
           RGB[2] = 0
+          newState.LEDS[lightindex].color = [0,0,0];
         }
     });
     //add the light to the array if its not in there yet
     if(addlight){        
       newState.article.lights.push({"lightNum":LightNum,"RGB": RGB})
-
+      let lightindex = newState.LEDS.findIndex (el => el.LightNum === LightNum)
+      newState.LEDS[lightindex].color = RGB;
     }
 
     //build single light json for that api
@@ -49,98 +53,101 @@ class App extends Component {
       //update state with current json
       this.setState(newState)
    }
+ 
+ 
+getlights = ()  =>  {
+    axios.get('http://192.168.1.245/getLights').then ((response) => {
+     
+      this.setupGrid(response);
+    
+    })
 
-componentDidMount()
-{
-  axios.get('http://192.168.1.245/getLights').then ((response) => {
-    let newState = this.state; 
-    newState.article = response.data.article;
-    let LedNum = 50
-    console.log(newState.article.lights);
-   if (newState.LightsLoaded == undefined)
+
+}
+setupGrid = (response)  => {
+  let newState = this.state; //Grab state which is where the JSON is saved
+  newState.article =  response.data.article;
+  let LedNum = 50
+  let color =[0,0,0];
+  let light;
+  newState.article =  response.data.article;
+
+  // only try to intialize
+  if (newState.LightsLoaded == undefined)
    {
-
-
     for (var x=11; x> 0; x--) {
 
-      if ( x % 2 == 0) //Even
+      if ( x % 2 == 0) //Even column
       {
         for(var y=18; y >0; y--)
          {     
-          newState.LEDS.push({"LightNum":LedNum,"x": x, "y":y})
+          light = newState.article.lights.find( ({lightNum}) =>lightNum === LedNum) ;
+          if(light != undefined && light.RGB != undefined)
+          {
+              color = light.RGB;
+          }
+          newState.LEDS.push({"LightNum":LedNum,"x": x, "y":y ,"color":color })
           LedNum ++
          }
       }
-      if ( x % 2 != 0) //Even
+      if ( x % 2 != 0) //ODD column
       {
         for(var y=1; y <19; y++)
          {     
-          newState.LEDS.push({"LightNum":LedNum,"x": x, "y":y})
+          light = newState.article.lights.find( item =>item.LightNum === LedNum) ;
+          if(light != undefined && light.RGB != undefined)
+          {
+            color = light.RGB;
+          }
+          newState.LEDS.push({"LightNum":LedNum,"x": x, "y":y ,"color":color })
           LedNum ++
          }
       }
     }
+
+    //order lights by coordinates to set the order the UI willend up using
+    newState.LEDS = _.orderBy(newState.LEDS, ['y','x'],['desc','asc']);
+    
+    //randomized code to simulate final wall REMOVE once we define the holes / angles
+    let degrees = 0;
+    let holdNumber = 1;
+    let counter = 1;
+    newState.LEDS.forEach(LED => {
+      if(counter % 5 == 0)
+      {
+          LED.holdimg = '../src/images/Hold'+holdNumber +'.png';
+          LED.holdNumber = holdNumber;
+          LED.coords = {"x":LED.x,"y":LED.y};
+          holdNumber++;
+          degrees = degrees +6;
+          if (holdNumber == 37)
+          {
+           holdNumber = 1;
+          }
+      }
+      counter++;
+    })
+    
     newState.LightsLoaded = 1;
     this.setState(newState)
   }
 }
-  );
+
+
+componentDidMount()
+{
+    this.getlights();
 }
 render ()
 {
- let  lightinfo = "" ;
- let grids= [] ;
- let degrees = 0;
-    
- let holdNumber = 1;
- let counter = 1;
-
-
-for (var i=18; i > 0; i--) {
-    for(var j=1; j < 12; j++)
-    {  
-      let holdimg = '' 
-      
-      lightinfo = this.state.LEDS.find(item => item.x === j && item.y === i);
-      if(lightinfo != undefined)
-      {
-        let light = this.state.article.lights.find( item => item.lightNum === lightinfo.LightNum) ;
-        let color =[0,0,0];
-        if(light != undefined)
-        {
-           if(light.RGB != undefined)
-           {
-            color = light.RGB;
-           }
-
-        }
-          if(counter % 5 == 0)
-          {
-               holdimg = '../src/images/Hold'+holdNumber +'.png';
-              holdNumber++;
-              if (holdNumber == 37)
-              {
-                holdNumber = 1;
-              }
-          }
-            grids.push({"colors": color, "coords":[j,i], "lightNum":lightinfo.LightNum, "degree":degrees, "holdimgs":holdimg});
-            degrees = degrees + 10
-            counter++
-      }
-
-     
-    };
-  
-};
   return(
     <div >
-           {grids.map(({ colors, coords, lightNum, degree,holdimgs }) => (
+           { this.state.LEDS.map(({ color, LightNum, degree,holdimg }) => (
         <GridButton
-          colors={colors}
-          coords={coords}
-          lightNum={lightNum}
+          colors={color}
+          lightNum={LightNum}
           degree={degree}
-          holdimg={holdimgs}
+          holdimg={holdimg}
           CallAPI={this.CallAPI.bind(this)}
         />
       ))}
--- src/GridButton.jsx
+++ src/GridButton.jsx
@@ -2,9 +2,9 @@ import React, { Component,useEffect } from 'react';
 
 
 class GridButton extends Component {
- // constructor(props) {
-  //  super(props);
- // }
+   constructor(props) {
+    super(props);
+  }
 
   apicall = ()  => {
     this.props.CallAPI([0,25,0],this.props.lightNum)
@@ -12,28 +12,31 @@ class GridButton extends Component {
 
 
 render(){
+    
+    //set the rotation value used for the hold
      let degree =  this.props.degree;
      let  rotation = 'rotate('+degree+'deg)translateX(-50%)';
-       console.log(this.props)
-      let cssclass = 'led_off'
+
+     //set CSS class of the indicator light
+     let cssclass;
       if (this.props.colors[1] > 0) 
         { cssclass = "led-green"} 
       else{cssclass = 'led-off'};
-      if (this.props.holdimg != '')
+
+      // if there is a holdimg present  load the div with the hold and indicator led div
+      if (this.props.holdimg != undefined)
       {
         return (
           <div class="button" style={{ cursor: "pointer"}} onClick={() => this.apicall()}>   
              <img src={this.props.holdimg} style={{transform: rotation}}/>   
-            <div  class={cssclass}></div>
+            <div  class={cssclass}></div> 
           </div>
 
          );
       }
+      // if there i no holdimg just create an empty div
       return (
-        <div class="button" >   
-
-        </div>
-
+        <div class="button" ></div>
        );
 
    }
